7b139b275545cb9f6b3d03959bf3034df83244e4
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2015-05-28 09:39:57 +0200

    added fixture method tests

---
+++ src/test/groovy/org/luksze/spock/FixtureMethodTest.groovy
@@ -0,0 +1,41 @@
+package org.luksze.spock
+
+import spock.lang.Specification
+
+import java.util.concurrent.atomic.AtomicInteger
+
+class FixtureMethodTest extends Specification {
+
+    def number = new AtomicInteger()
+    static staticNumber = new AtomicInteger()
+
+    def setupSpec() {
+        println "before specification"
+        staticNumber.incrementAndGet()
+    }
+
+    def setup() {
+        println "before method"
+        number.incrementAndGet()
+    }
+
+    def "test method"() {
+        println "first test method"
+        expect: number.incrementAndGet() == 2
+        and: staticNumber.incrementAndGet() == 2
+    }
+
+    def "other test method"() {
+        println "second test method"
+        expect: number.incrementAndGet() == 2
+        and: staticNumber.incrementAndGet() == 3
+    }
+
+    def cleanup() {
+        println "cleanup method"
+    }
+
+    def cleanupSpec() {
+        println "after specification"
+    }
+}
