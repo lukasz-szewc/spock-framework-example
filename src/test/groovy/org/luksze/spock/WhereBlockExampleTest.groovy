6e388022d24c89348dcf9c1b2a9d3c0cb7ab92b4
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2015-05-29 13:35:23 +0200

    added unroll example

--- src/test/groovy/org/luksze/spock/WhereBlockExampleTest.groovy
+++ src/test/groovy/org/luksze/spock/WhereBlockExampleTest.groovy
@@ -1,8 +1,11 @@
 package org.luksze.spock
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class WhereBlockExampleTest extends Specification {
+
+    @Unroll
     def "where block Example"() {
         expect:
         Math.max(a, b) == c
@@ -11,5 +14,20 @@ class WhereBlockExampleTest extends Specification {
         a << [5, 1, -1]
         b << [6, 2, 33]
         c << [6, 2, 33]
+    }
+
+    @Unroll
+    def "maximum of two numbers #a #b #c"() {
+        expect: Math.max(a, b) == c
+
+        where:
+        a | b | c
+        1 | 3 | 3
+        4 | 5 | 5
+        9 | 1 | 9
+    }
+
+    void setup() {
+        println 'setup'
     }
 }
